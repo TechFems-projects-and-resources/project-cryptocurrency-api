dd4bc0bfc0c2554f2e09663680023b6bc726e066
bipasha39 <physiobipasha39@gmail.com>
2021-09-30 14:00:47 +0200

    project done

--- src/App.js
+++ src/App.js
@@ -42,8 +42,10 @@ function App() {
             name={coin.name}
             price={coin.current_price}
             symbol={coin.symbol}
-            volume={coin.market_cap}
+            volume={coin.total_volume}
             image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+            marketcap={coin.market_cap}
             
           />
         );
